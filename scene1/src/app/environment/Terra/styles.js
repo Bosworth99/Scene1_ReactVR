9abfd632b53b709a81bd01f5dd0e12966c35ca84
Josh Bosworth <josh@joshbosworth.com>
2018-01-08 16:57:17 -0500

    Revert "Dev // basic scene"

--- scene1/src/app/environment/Terra/styles.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StyleSheet } from 'react-vr';
-
-const styles = StyleSheet.create({
-    container: {},
-});
-
-export default styles;
--- scene1/src/app/styles.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { StyleSheet } from 'react-vr';
-
-const styles = StyleSheet.create({
-    container: {},
-    content: {},
-});
-
-export default styles;
